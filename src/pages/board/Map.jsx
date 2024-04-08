41b14b6189e63fce9dcc53a608033e498079654d
lovemagician <ddiddibboddiddibbo@gmail.com>
2024-04-08 22:30:44 +0900

    ✨ Feat: 지도 생성

--- src/pages/board/Map.jsx
+++ src/pages/board/Map.jsx
@@ -1,9 +1,22 @@
-function Map() {
-  return (
-    <>
-      <h1>Map</h1>
-    </>
-  );
-}
+import { useEffect } from 'react';
+const { kakao } = window;
 
-export default Map;
+const App = () => {
+  useEffect(() => {
+    const container = document.getElementById('map');
+    const options = {
+      center: new kakao.maps.LatLng(33.450701, 126.570667),
+      level: 3,
+    };
+
+    const map = new kakao.maps.Map(container, options);
+  }, []);
+
+  return (
+    <div>
+      <div id="map" style={{ width: '500px', height: '400px' }} />
+    </div>
+  );
+};
+
+export default App;
