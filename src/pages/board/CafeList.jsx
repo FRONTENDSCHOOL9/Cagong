9a1a8638f3c3e1b8b6be81a2405e81f50cab9749
loeybho <byo_l@naver.com>
2024-04-09 18:23:39 +0900

    📝 file 기본 구조 설정

--- src/pages/board/CafeList.jsx
+++ src/pages/board/CafeList.jsx
@@ -1,11 +1,35 @@
-import Header from '@components/layout/Header';
 import { Link } from 'react-router-dom';
+import useCustomAxios from '@hooks/useCustomAxios.mjs';
+import { useEffect, useState } from 'react';
 
 function CafeList() {
+  const [mainImages, setMainImages] = useState([]); // 이미지 배열 저장
+  const [name, setName] = useState([]);
+  const axios = useCustomAxios();
+
+  useEffect(() => {
+    axios.get(`${import.meta.env.VITE_API_SERVER}/products/6`).then(res => {
+      console.log(res.data.item.name);
+      setMainImages(res.data.item.mainImages);
+      setName(res.data.item.name);
+    });
+  }, []);
+
   return (
     <>
       <h1>CafeList</h1>
-      <Header />
+      {name}
+
+      {mainImages.length > 0 && (
+        <ul>
+          {mainImages.map(image => (
+            <li key={image.originalname}>
+              {image.name}
+              <img src="{image.path}" />
+            </li>
+          ))}
+        </ul>
+      )}
       <Link to="/boards/cafedetail">cafedetail</Link>
     </>
   );
