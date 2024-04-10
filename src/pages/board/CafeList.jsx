7f894bbb4b9db047c26466b65530ae7e33ecd2de
loeybho <byo_l@naver.com>
2024-04-10 16:51:40 +0900

    🚧 Feat: cafelist 구현 중

--- src/pages/board/CafeList.jsx
+++ src/pages/board/CafeList.jsx
@@ -1,17 +1,56 @@
 import { Link } from 'react-router-dom';
 import useCustomAxios from '@hooks/useCustomAxios.mjs';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
+import styled from 'styled-components';
+
+const MyComponent = styled.div`
+  width: 100%;
+  ul {
+    margin: 0;
+    padding: 0;
+    list-style: none;
+  }
+  h1 {
+    margin: 0;
+    padding: 0;
+
+    text-align: center;
+  }
+  img {
+    margin: 0;
+    padding: 0;
+  }
+
+  .cafe-thumb {
+    width: 100%;
+    height: 100%;
+    border-radius: 20px;
+    aspect-ratio: 1/1;
+  }
+
+  a {
+    text-decoration: none;
+  }
+`;
 
 function CafeList() {
   const [data, setData] = useState([]);
+  const [isBookmarked, setIsBookmarked] = useState(false);
   const axios = useCustomAxios();
+
+  const handleBookmark = () => {
+    axios.post(`${import.meta.env.VITE_API_SERVER}/bookmarks`).then(res => {
+      setIsBookmarked(!isBookmarked);
+      console.log(isBookmarked);
+      console.log(res);
+    });
+  };
 
   useEffect(() => {
     axios.get(`${import.meta.env.VITE_API_SERVER}/products`).then(res => {
-      // console.log(res.data.item);
       const items = res.data.item;
       for (let i = 0; i < items.length; i++) {
-        console.log(items[i].mainImages);
+        // console.log(items[i]);
         setData(items);
       }
     });
@@ -19,26 +58,40 @@ function CafeList() {
 
   return (
     <>
-      <h1>카공 인기카페</h1>
-      <ul>
-        {data.map(item => (
-          <li key={item._id}>
-            <div>
+      <MyComponent className="cafelist">
+        <h1 className="title">카공 인기카페</h1>
+        <ul>
+          {data.map(item => (
+            <li key={item._id}>
               <img
-                src={
-                  import.meta.env.VITE_API_SERVER +
-                  '/files/05-cagong/' +
-                  item.mainImages[0].name
-                }
-                alt="카페사진"
+                src="../public/bookmark.svg"
+                type="button"
+                onClick={handleBookmark}
+                alt="북마크 버튼"
               />
-            </div>
-            <Link to={`/boards/cafeDetail/${item._id}`}>{item.name}</Link>
-            {item.content}
-            <div>{item.bookmarks}</div>
-          </li>
-        ))}
-      </ul>
+              <Link key={item._id} to={`/boards/cafeDetail/${item._id}`}>
+                <div>
+                  <img
+                    className="cafe-thumb"
+                    src={
+                      import.meta.env.VITE_API_SERVER +
+                      '/files/05-cagong/' +
+                      item.mainImages[0].name
+                    }
+                    alt="카페사진"
+                  />
+                </div>
+                {item.name}
+                <div>{item.extra.address}</div>
+                <div>
+                  <img src="../public/stars.svg" />
+                  리뷰 {item.replies}
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </MyComponent>
     </>
   );
 }
