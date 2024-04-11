844d274e42d502ea30db75470c55b09562c3f0b0
do990118 <doe990118@naver.com>
2024-04-11 09:11:53 +0900

    🚧 Spinner.jsx 추가, 적용

--- src/pages/board/CafeDetail.jsx
+++ src/pages/board/CafeDetail.jsx
@@ -11,36 +11,31 @@ import Button from '@components/button/Button';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { memberState } from '@recoil/user/atoms.mjs';
+import { useQuery } from '@tanstack/react-query';
 
 function CafeDetail() {
   const axios = useCustomAxios();
   let { _id } = useParams();
+  const user = useRecoilValue(memberState);
   const [isOrdered, setIsOrdered] = useState(false);
   const navigate = useNavigate();
 
-  const [data, setData] = useState();
-  const [review, setReview] = useState();
-
-  async function getProducts() {
-    const response = await axios.get(`/products/${_id}`);
-    setData(response.data);
-  }
-
-  useEffect(() => {
-    getProducts();
-  }, []);
+  const { data } = useQuery({
+    queryKey: ['products', _id],
+    queryFn: () => axios.get(`/products/${_id}`),
+    select: response => response.data,
+    suspense: true,
+  });
 
   // useEffect(() => {
   //   // 버튼 클릭 시 로컬 스토리지에 isOrdered 값 저장
   //   localStorage.setItem('isOrdered', isOrdered.toString());
   // }, [isOrdered]);
 
-  const user = useRecoilValue(memberState);
-
   const confirmUser = () => {
     if (!user) {
       const gotoLogin = confirm(
-        '로그인 후 이용 가능합니다. \n 로그인 화면으로 이동하시겠습니까?',
+        '로그인 후 이용 가능합니다.\n로그인 화면으로 이동하시겠습니까?',
       );
       gotoLogin && navigate('/users/login');
     } else {
@@ -68,6 +63,8 @@ function CafeDetail() {
     }
   }
 
+  const [review, setReview] = useState();
+
   async function getReview() {
     const response = await axios.get(`/replies/${_id - 1}`);
     setReview(response.data);
@@ -77,21 +74,35 @@ function CafeDetail() {
     getReview();
   }, []);
 
+  // const { review } = useQuery({
+  //   queryKey: ['replies', _id],
+  //   queryFn: () => axios.get(`/replies/${_id - 1}`),
+  //   select: response => response.data,
+  //   suspense: true,
+  // })
+
   return (
     <>
       <Swiper
-        // install Swiper modules
+        style={{
+          '--swiper-navigation-color': '#828282',
+          '--swiper-pagination-color': '#828282',
+        }}
+        lazy={true}
         modules={[Navigation, Pagination, Scrollbar, A11y]}
         spaceBetween={10}
         slidesPerView={3}
-        navigation
-        pagination={{ clickable: true }}
-        // scrollbar={{ draggable: true }}
+        navigation={true}
+        loop={true}
+        pagination={{ 
+          clickable: true
+         }}
+        centeredSlides={true}
       >
-        {data?.item.mainImages?.map((image, index) => (
+        {data.item.mainImages?.map((image, index) => (
           <SwiperSlide key={index}>
             <img
-              style={{ width: '300px', height: '300px' }}
+              style={{ width: '300px', height: '300px', objectFit: 'cover' }}
               src={`${import.meta.env.VITE_API_SERVER}/files/05-cagong/${
                 image.name
               }`}
@@ -100,7 +111,7 @@ function CafeDetail() {
           </SwiperSlide>
         ))}
       </Swiper>
-      <Link to="/boards/map">{data?.item.extra.address}</Link>
+      <Link to="/boards/map">{data.item.extra.address}</Link>
       <button>복사하기</button>
 
       <h2>카공단 제공 메뉴</h2>
@@ -119,7 +130,7 @@ function CafeDetail() {
       </Button>
       <h2>방문자 리뷰</h2>
       {review?.item.map(item => (
-        <div key={item._id}>
+        <div key={_id}>
           <p>{item.createdAt}</p>
           <p>{item.content}</p>
         </div>
