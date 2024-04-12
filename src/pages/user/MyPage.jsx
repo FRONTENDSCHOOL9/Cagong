3e68eb09984f6d38afad481d464f45c36d7a4e05
noohijimik <noohijimik00@gmail.com>
2024-04-12 18:02:08 +0900

    🚧 Feat: Mypage 구현 중

--- src/pages/user/MyPage.jsx
+++ src/pages/user/MyPage.jsx
@@ -1,37 +1,15 @@
-import { useEffect, useState } from 'react';
+import { memberState } from '@recoil/user/atoms.mjs';
+import { useRecoilValue } from 'recoil';
 import { Link } from 'react-router-dom';
-import useCustomAxios from '@hooks/useCustomAxios.mjs';
-import { useParams } from 'react-router-dom';
 
 function MyPage() {
-  const axios = useCustomAxios();
-  const { _id } = useParams();
-  const [userInfo, setUserInfo] = useState([]);
-
-  useEffect(() => {
-    axios.get(`${import.meta.env.VITE_API_SERVER}/users/${_id}`).then(res => {
-      console.log(res);
-      console.log(res.config.headers.Authorization);
-      setUserInfo(res.data.item);
-    });
-  }, []);
+  const user = useRecoilValue(memberState);
+  console.log(user);
 
   return (
     <div>
-      {userInfo.name && (
-        <div>
-          <Link to="/users/reviewlist">리뷰 리스트</Link>
-          <img src="/assets/byeol.jpg" alt="" />
-          <p>
-            {userInfo.name}님 안녕하세요! 페이지 주소는 {userInfo._id}
-          </p>
-          <p>카공 병아리</p>
-          <h1>최근 조회한 카페</h1>
-          <p>리뷰관리</p>
-          <p>공지사항</p>
-          <p>1:1 문의하기</p>
-        </div>
-      )}
+      <text>Mypage</text>
+      {user && <div>정보</div>}
     </div>
   );
 }
