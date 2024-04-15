3a5754d5fb37733f4409ddc88ef6ef6757995e5c
loeybho <byo_l@naver.com>
2024-04-15 09:09:42 +0900

    📦️ Package: immer 추가

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
       { find: '@pages', replacement: '/src/pages' },
       { find: '@hooks', replacement: '/src/hooks' },
       { find: '@recoil', replacement: '/src/recoil' },
+      { find: '@assets', replacement: '/src/assets' },
     ],
   },
 });
