94b4d2f0b3b8a4af6f81b8831e00a46ec04f329e
noohijimik <noohijimik00@gmail.com>
2024-04-16 18:00:37 +0900

    🚧 Feat: Mypage 최근 조회한 카페 리스트 구현중

--- src/pages/user/MyPage.jsx
+++ src/pages/user/MyPage.jsx
@@ -1,15 +1,42 @@
+import { useState, useEffect } from 'react';
 import { memberState } from '@recoil/user/atoms.mjs';
 import { useRecoilValue } from 'recoil';
+import PropTypes from 'prop-types';
+import useCustomAxios from '@hooks/useCustomAxios.mjs';
 import { Link } from 'react-router-dom';
+import CafeListItem from '@pages/board/CafeListItem';
+import { useQuery } from '@tanstack/react-query';
 
 function MyPage() {
+  const axios = useCustomAxios();
+  const { data } = useQuery({
+    queryKey: ['products'],
+    queryFn: () => axios.get('/products'),
+    select: response => response.data,
+    suspense: true,
+  });
+  console.log(data.item[0].name);
+
   const user = useRecoilValue(memberState);
-  console.log(user);
+
+  const [viewedCafes, setViewedCafes] = useState([]);
+
+  useEffect(() => {
+    // localStorage에서 저장된 최근 조회한 카페 id
+    const storedViewedCafes = localStorage.getItem('viewedCafeIds');
+
+    // 저장된 id로 상태 업데이트
+    if (storedViewedCafes) {
+      setViewedCafes(JSON.parse(storedViewedCafes));
+    }
+  }, []);
 
   return (
     <div>
       <text>Mypage</text>
       {user && <div>정보</div>}
+      {user.name}
+      <div className="cafelist-title">최근 조회한 카페</div>
     </div>
   );
 }
