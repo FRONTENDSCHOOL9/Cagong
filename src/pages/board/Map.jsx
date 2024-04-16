203b168f5fa790b7088b02ac31986ed402200c52
lovemagician <ddiddibboddiddibbo@gmail.com>
2024-04-16 18:03:16 +0900

    🚧 Feat: 사용자 위치 기준 카페까지의 거리 표시 기능 구현

--- src/pages/board/Map.jsx
+++ src/pages/board/Map.jsx
@@ -2,18 +2,137 @@ import useCustomAxios from '@hooks/useCustomAxios.mjs';
 import { useEffect, useRef, useState } from 'react';
 const { kakao } = window;
 import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
 function Map() {
   const mapRef = useRef(null);
   const infowindowRef = useRef(null);
-  const axios = useCustomAxios();
+  const curLatRef = useRef(null);
+  const curLonRef = useRef(null);
+  const distanceToCafeRef = useRef(null);
+  const dataRef = useRef(null);
+  const customAxios = useCustomAxios();
 
   const { data } = useQuery({
     queryKey: ['products'],
-    queryFn: () => axios.get(`/products`),
+    queryFn: () => customAxios.get(`/products`),
     select: response => response.data,
     suspense: true,
   });
+  dataRef.current = data;
+  currentLocation();
+  function currentLocation() {
+    // HTML5의 geolocation으로 사용할 수 있는지 확인
+
+    if (navigator.geolocation) {
+      // GeoLocation을 이용해서 접속 위치를 얻음
+      navigator.geolocation.getCurrentPosition(async function (position) {
+        const lat = position.coords.latitude; // 위도
+        const lon = position.coords.longitude; // 경도
+        curLatRef.current = lat;
+        curLonRef.current = lon;
+        // console.log(curLonRef.current, curLatRef.current);
+      });
+    } else {
+      // HTML5의 GeoLocation을 사용할 수 없을때 처리
+      alert('현재 위치를 찾을 수 없습니다.');
+    }
+  }
+
+  // KakaoAK ${import.meta.env.VITE_KAKAO_REST_API_KEY}
+
+  useEffect(() => {
+    //첫번 째는 받아오는 주소, 두,세 번째는 현재 위치 (좌표)
+    async function getDistanceBetweenTwoAddresses(addr, curLon, curLat) {
+      try {
+        //입력한 첫 주소를 WTM 좌표로 변환
+        const response1 = await axios.get(
+          'https://dapi.kakao.com/v2/local/search/address.json',
+          {
+            params: { query: addr },
+            headers: {
+              Authorization: `KakaoAK ${
+                import.meta.env.VITE_KAKAO_REST_API_KEY
+              }`,
+            },
+          },
+        );
+
+        const { x: x1, y: y1 } = response1.data.documents[0];
+
+        const wtmResponse1 = await axios.get(
+          'https://dapi.kakao.com/v2/local/geo/transcoord.json',
+          {
+            params: { x: x1, y: y1, output_coord: 'WTM' },
+            headers: {
+              Authorization: `KakaoAK ${
+                import.meta.env.VITE_KAKAO_REST_API_KEY
+              }`,
+            },
+          },
+        );
+
+        const { x: wtmX1, y: wtmY1 } = wtmResponse1.data.documents[0];
+        // console.log(wtmResponse1.data.documents[0]);
+
+        const wtmResponse2 = await axios.get(
+          'https://dapi.kakao.com/v2/local/geo/transcoord.json',
+          {
+            params: {
+              x: curLon ? curLon : 0,
+              y: curLat ? curLat : 0,
+              input_coord: 'WGS84',
+              output_coord: 'WTM',
+            },
+            headers: {
+              Authorization: `KakaoAK ${
+                import.meta.env.VITE_KAKAO_REST_API_KEY
+              }`,
+            },
+          },
+        );
+
+        const { x: wtmX2, y: wtmY2 } = wtmResponse2.data.documents[0];
+
+        // console.log(wtmResponse2.data.documents[0]);
+        //최단거리 계산 후 distance에 담음
+        const distance = calculateDistance(wtmX1, wtmY1, wtmX2, wtmY2);
+        //미터 단위를 1km부터 킬로미터 단위로 변환
+        const formattedDistance =
+          distance <= 1000
+            ? parseInt(distance / 1000)
+            : parseInt(distance / 1000);
+        return formattedDistance;
+      } catch (error) {
+        console.error('주소를 가져오지 못 했습니다.', error);
+        return null;
+      }
+    }
+
+    // 하버사인 공식으로 최단거리 계산하는 함수
+    function calculateDistance(x1, y1, x2, y2) {
+      return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
+    // console.log(data?.item[0].extra.address);
+
+    const distanceArray = [];
+    data?.item?.map(async item => {
+      await getDistanceBetweenTwoAddresses(
+        item.extra.address,
+        curLonRef.current,
+        curLatRef.current,
+      ).then(res =>
+        distanceArray.push({
+          res: res,
+          _id: item._id,
+        }),
+      );
+
+      distanceToCafeRef.current = distanceArray;
+
+      // console.log(distanceToCafeRef.current);
+    });
+  }, [curLonRef.current, curLatRef.current]);
 
   const [filteredCafeList, setFilteredCafeList] = useState([]);
 
@@ -191,8 +310,7 @@ function Map() {
         const lat = position.coords.latitude; // 위도
         const lon = position.coords.longitude; // 경도
         const locPosition = new kakao.maps.LatLng(lat, lon); // geolocation으로 얻어온 좌표
-
-        mapRef.current.setLevel(4);
+        mapRef.current.setLevel(4, { animate: true });
         mapRef.current.panTo(locPosition); // geolocation으로 얻어온 좌표로 이동
       });
     } else {
@@ -200,6 +318,11 @@ function Map() {
       alert('현재 위치를 찾을 수 없습니다.');
     }
   }
+  useEffect(() => {
+    setTimeout(() => {
+      handleCurrentLocation(); // 페이지 렌더 3초 후 현재 위치로
+    }, 3000);
+  }, []);
 
   //초기 위치로 돌아가는 함수
   function handleInitLocation() {
@@ -208,7 +331,7 @@ function Map() {
       127.76185524802845,
     );
     mapRef.current.panTo(initPosition);
-    mapRef.current.setLevel(15);
+    mapRef.current.setLevel(15, { animate: true });
     // mapRef.current.setCenter(36.349396783484984, 127.76185524802845);
 
     // center: new kakao.maps.LatLng(36.349396783484984, 127.76185524802845), // 지도의 중심좌표
@@ -234,14 +357,15 @@ function Map() {
     };
   }
 
+  useEffect(() => {}, [distanceToCafeRef.current]);
   // 카페 리스트 받아오기
+
   const allCafeList = data?.item?.map(item => (
     <li
       style={{ cursor: 'pointer', width: '200px' }}
       key={item._id}
       onClick={handleSelectLocation(item)}
     >
-      {item.name}
       <img
         style={{ width: '200px' }}
         src={
@@ -251,17 +375,23 @@ function Map() {
         }
         alt="카페사진"
       />
+      <span>{item.name}</span>
+      <span style={{ fontSize: '12px' }}>
+        {distanceToCafeRef.current?.map(distance =>
+          item._id === distance._id ? `${distance.res}km` : '',
+        )}
+      </span>
     </li>
   ));
 
   //카페 리스트 받아오기
+
   const changedCafeList = filteredCafeList?.map(item => (
     <li
       style={{ cursor: 'pointer', width: '200px' }}
       key={item._id}
       onClick={handleSelectLocation(item)}
     >
-      {item.name}
       <img
         style={{ width: '200px' }}
         src={
@@ -271,8 +401,19 @@ function Map() {
         }
         alt="카페사진"
       />
+      <span>{item.name}</span>
+      <span style={{ fontSize: '12px' }}>
+        {distanceToCafeRef.current?.map(distance =>
+          item._id === distance._id ? `${distance.res}km` : '',
+        )}
+      </span>
     </li>
   ));
+  // .map(item =>
+  //   console.log(
+  //     parseInt(item.props.children[2].props.children.filter(el => el)),
+  //   ),
+  // );
   // console.log(changedCafeList);
 
   return (
