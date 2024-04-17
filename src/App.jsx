a123b36b31dbed14af8a530b275de956262c7d92
loeybho <byo_l@naver.com>
2024-04-17 16:29:39 +0900

    💄 Styles: 웹사이트 반응형 UI 개선

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import { Suspense } from 'react';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Spinner from '@components/Spinner';
-
 const queryClient = new QueryClient();
 
 function App() {
