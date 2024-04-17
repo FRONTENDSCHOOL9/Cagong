33901bf9e57b9f302c3387cec1cdab64ed3770b6
loeybho <byo_l@naver.com>
2024-04-17 16:42:11 +0900

    🐛 Fix: <Wrapper> 경로 수정

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,7 +1,7 @@
 import Header from '@components/layout/Header';
 import Navbar from '@components/layout/Navbar';
 import { Outlet } from 'react-router-dom';
-import Wrapper from '@/Wrapper.jsx';
+import Wrapper from '@components/Wrapper.jsx';
 function Layout() {
   return (
     <div>
