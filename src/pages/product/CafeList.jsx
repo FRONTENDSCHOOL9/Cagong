3d3ca54cee2f9a3f5735448d1fc51c417adfade7
loeybho <byo_l@naver.com>
2024-04-21 15:34:45 +0900

    ✨ Feat: CafeList 무한스크롤 추가

--- src/pages/product/CafeList.jsx
+++ src/pages/product/CafeList.jsx
@@ -1,10 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
 import useCustomAxios from '@hooks/useCustomAxios.mjs';
 import styled from 'styled-components';
-
 import MainHeader from '@components/layout/MainHeader';
 import CafeListItem from '@pages/product/CafeListItem';
 import Wrapper from '@components/layout/Wrapper';
+import { useInfiniteQuery } from '@tanstack/react-query';
+import InfiniteScroll from 'react-infinite-scroller';
+import _ from 'lodash';
 
 const MyComponent = styled.div`
   .cafelist-title {
@@ -18,16 +19,40 @@ const MyComponent = styled.div`
 function CafeList() {
   const axios = useCustomAxios();
 
-  const { data } = useQuery({
+  const { data, fetchNextPage, isFetching } = useInfiniteQuery({
     queryKey: ['cafelist'],
-    queryFn: () => axios.get('/products'),
-    select: response => response.data.item,
-    suspense: true,
+    queryFn: ({ pageParam = 1 }) =>
+      axios.get('/products', {
+        params: {
+          page: pageParam,
+          limit: import.meta.env.VITE_CAFELIST,
+          sort: JSON.stringify({ _id: -1 }),
+        },
+      }),
+    select: response => {
+      response.items = response.pages.map(page => page.data.item);
+      response.totalPages = response.pages.at(-1).data.pagination.totalPages;
+      response.page = response.pages.at(-1).data.pagination.page;
+      return response;
+    },
+    getNextPageParam: lastPage => {
+      const pagination = lastPage.data.pagination;
+      let nextPage =
+        pagination.page < pagination.totalPages ? pagination.page + 1 : false;
+      return nextPage;
+    },
   });
 
-  const cafeList = data?.map(item => (
-    <CafeListItem key={item._id} item={item} />
-  ));
+  console.log(data);
+
+  let cafeList = [];
+  let hasNext = false;
+  if (data) {
+    cafeList = _.flatten(data.items).map(item => {
+      return <CafeListItem key={item._id} item={item} />;
+    });
+    hasNext = data.page < data.totalPages;
+  }
 
   return (
     <>
@@ -35,7 +60,13 @@ function CafeList() {
       <Wrapper>
         <MyComponent>
           <h1 className="cafelist-title">카공 인기카페</h1>
-          {cafeList}
+          <InfiniteScroll
+            pageStart={1}
+            loadMore={fetchNextPage}
+            hasMore={!isFetching && hasNext}
+          >
+            {cafeList}
+          </InfiniteScroll>
         </MyComponent>
       </Wrapper>
     </>
